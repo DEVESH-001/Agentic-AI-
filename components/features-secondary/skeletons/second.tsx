b468790f4ce907e0c52b6f70cb633d7f69ee99f9
DEVESH-001 <dd4168@srmist.edu.in>
2026-01-10 22:56:16 +0530

    Improved the revolving-card-comp

--- components/features-secondary/skeletons/second.tsx
+++ components/features-secondary/skeletons/second.tsx
@@ -3,23 +3,47 @@
 import { Logo } from "@/components/logo";
 import { ChatGptIcon, GoogleSheetsIcon, InstaIcon, MetaIcon } from "@/icons";
 import { cn } from "@/lib/utils";
+import { IconPrison } from "@tabler/icons-react";
 import { motion } from "motion/react";
 
 export const SkeletonTwo = () => {
   return (
-    <div className="absolute inset-x-0 mx-auto flex h-full w-full flex-1 items-center justify-center gap-3 rounded-t-3xl p-2">
+    <div
+      className="absolute inset-x-0 mx-auto flex h-full w-full flex-1 items-center justify-center gap-3 rounded-t-3xl p-2"
+      style={{transform:"rotateY(20deg) rotateX(20deg) rotateZ(-20deg)"}}
+    >
+      {/* this part is to create revolving cards around a center logo */}
       <Circle className="flex items-center justify-center border-neutral-200 shadow-sm">
+        {/* center logo */}
         <Logo className="size-10" />
 
         <RevolvingCard>
-          <MetaIcon />
+          <MetaIcon className="size-8 text-blue-500" />
         </RevolvingCard>
 
-        <RevolvingCard>
-          <GoogleSheetsIcon />
+        <RevolvingCard className="[--orbit-duration:20s] [--translate-position:140px]">
+          <GoogleSheetsIcon className="size-6 text-green-500" />
         </RevolvingCard>
-        
+
+        <RevolvingCard className="[--orbit-duration:15s] [--translate-position:140px]">
+          <ChatGptIcon className="size-8 text-neutral-700" />
+        </RevolvingCard>
+
+        <RevolvingCard className="[--orbit-duration:15s] [--translate-position:140px]">
+          <InstaIcon className="size-8 text-red-500" />
+        </RevolvingCard>
+
+        <RevolvingCard className="size-auto w-60 bg-transparent shadow-none ring-0 [--orbit-duration:30s] [--translate-position:250px]">
+          <SkeletonCard
+            className="absolute bottom-20 left-4 z-10 max-w-[80%]"
+            icon={<IconPrison className="size-4" />}
+            title="Risk Analysis"
+            description="Creates clear, ready-to-use campaign briefs using product info, audience data, and past results."
+          />
+        </RevolvingCard>
+        {/* end revolving cards */}
       </Circle>
+      {/*  surrounding circles for depth effect */}
       <Circle className="z-9 size-60 border-neutral-100 bg-neutral-100/80 shadow"></Circle>
       <Circle className="z-8 size-80 border-neutral-100 bg-neutral-100/60 shadow"></Circle>
       <Circle className="z-7 size-100 border-neutral-100 bg-neutral-100/40 shadow"></Circle>
@@ -62,6 +86,40 @@ const Circle = ({
       )}
     >
       {children}
+    </div>
+  );
+};
+
+const SkeletonCard = ({
+  icon,
+  title,
+  description,
+  badge,
+  className,
+}: {
+  icon?: React.ReactNode;
+  title?: string;
+  description?: string;
+  badge?: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto my-auto h-fit w-full max-w-[85%] rounded-2xl border border-neutral-200 bg-neutral-100 p-3 shadow-2xl dark:border-neutral-700 dark:bg-neutral-800",
+        className,
+      )}
+    >
+      <div className="flex items-center gap-3">
+        {icon}
+        <p className="text-sm font-normal text-black dark:text-white">
+          {title}
+        </p>
+        {badge}
+      </div>
+      <p className="mt-3 text-sm font-medium text-neutral-400/80 dark:text-neutral-400">
+        {description}
+      </p>
     </div>
   );
 };
--- icons/index.tsx
+++ icons/index.tsx
@@ -372,21 +372,23 @@ export const MetaIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
 };
 
 export const InstaIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="tabler-icon tabler-icon-brand-instagram size-8 text-red-500"
-    {...props}
-  >
-    <path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
-    <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-    <path d="M16.5 7.5v.01"></path>
-  </svg>;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="tabler-icon tabler-icon-brand-instagram size-8 text-red-500"
+      {...props}
+    >
+      <path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
+      <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+      <path d="M16.5 7.5v.01"></path>
+    </svg>
+  );
 };
